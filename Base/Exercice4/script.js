510d804ded2bd5be85a63e90cc1af194c98f7a92
Emmanuel G <stdgeld@gmail.com>
2024-11-22 10:19:43 +0100

    Exercice 4 with data attributes

--- Base/Exercice4/script.js
+++ Base/Exercice4/script.js
@@ -4,32 +4,16 @@ const container = document.querySelector('.container');
 squares.forEach((square) => { // for each of the .square element
     square.addEventListener('click', () => { // add a click listener
         
-        squares.forEach((sq) => sq.classList.remove("clicked")); // remove any already clicked class
+        if(square.classList.contains("active")) { // if the box is already active
+            square.classList.remove("active"); // remove active class
+            container.style.backgroundColor = "#708090"; // set bg color to gray
 
-        if(container.classList[1] == undefined) { // If the container doesn't have color yet
-                container.classList.add(square.classList[1]) // add the right color
-                square.classList.add("clicked") // add the clicked class to the box
-
-            } else if(container.classList[1] == square.classList[1]) { // if reclicking on the activated box
-                container.classList.remove(square.classList[1]) // remove the color
-
-            } else { // Else (ie if clicking on a second box while one is active)
-                container.classList.remove(container.classList[1]) // remove current bg color
-                container.classList.add(square.classList[1]) // add new color
-                square.classList.add("clicked") // add clicked class
-            }
+        } else { // else (ie if the box is not active)
+            squares.forEach(s => s.classList.remove("active")); // remove any active class
+            square.classList.add("active"); // add active class to current box
+            container.style.backgroundColor = square.dataset.color; // set proper bg color
+        }
     })
 });
 
-
-// works if the user is kind enough to deactivate each box before clicking the next
-
-// squares.forEach((square) => { // for each of the .square element
-//     square.addEventListener('click', () => { // add a click listener
- 
-//         container.classList.toggle(square.classList[1])
-//         square.classList.toggle("clicked")
-        
-//     })
-// });
 
